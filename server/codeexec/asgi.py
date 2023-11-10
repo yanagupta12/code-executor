a9d80948fb1affd324663570a6f5e65fb10e514c
Yana-Gupta <yanagupta897@gmail.com>
2023-11-10 21:39:12 +0530

    adder dockerfiles

--- server/codeexec/asgi.py
+++ server/codeexec/asgi.py
@@ -2,20 +2,13 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.routing import ProtocolTypeRouter
 
-import collaborate.routing
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'codeexec.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
-        URLRouter(
-            collaborate.routing.websocket_urlpatterns
-        )
-    )
 })
 
--- server/codeexec/settings.py
+++ server/codeexec/settings.py
@@ -28,7 +28,6 @@ CORS_ALLOWED_ORIGIN_REGEXES = [
 
 
 INSTALLED_APPS = [
-    'channels',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
