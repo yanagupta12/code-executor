d4e2534989cdb90d491c91af66dcfb7e0034bc11
Yana-Gupta <yanagupta897@gmail.com>
2023-11-10 22:31:48 +0530

    [ui] commented unused variables

--- client/src/components/CollaborativeEditor/index.tsx
+++ client/src/components/CollaborativeEditor/index.tsx
@@ -5,7 +5,7 @@ const Room = () => {
   const { room_id } = useParams()
   const [roomData, setRoomData] = useState(null)
   const sanitizedRoomId = room_id!.replace(/^"(.*)"$/, '$1')
-  const [socket, setSocket] = useState<WebSocket>()
+  // const [socket, setSocket] = useState<WebSocket>()
 
   useEffect(() => {
     const URL = 'http://localhost:8000/collaborate/get-room-by-id?room_id='
@@ -29,30 +29,30 @@ const Room = () => {
 
 
 
-  useEffect(() => {
-    // Socket URL
-    const socketUrl = "ws://localhost:8000/ws/me/"
-    console.log(socketUrl + room_id)
-    const newSocket = new WebSocket(socketUrl );
-    setSocket(newSocket);
+  // useEffect(() => {
+  //   // Socket URL
+  //   const socketUrl = "ws://localhost:8000/ws/me/"
+  //   console.log(socketUrl + room_id)
+  //   const newSocket = new WebSocket(socketUrl );
+  //   setSocket(newSocket);
 
-    newSocket.addEventListener('open', (event) => {
-      console.log("Websocket Connection established 🥳")
-      console.log('WebSocket connection opened:', event);
-    });
+  //   newSocket.addEventListener('open', (event) => {
+  //     console.log("Websocket Connection established 🥳")
+  //     console.log('WebSocket connection opened:', event);
+  //   });
 
-    newSocket.addEventListener('close', (event) => {
-      console.log('WebSocket connection closed:', event);
-    });
+  //   newSocket.addEventListener('close', (event) => {
+  //     console.log('WebSocket connection closed:', event);
+  //   });
 
-    newSocket.addEventListener('error', (error) => {
-      console.error('WebSocket error:', error);
-    });
+  //   newSocket.addEventListener('error', (error) => {
+  //     console.error('WebSocket error:', error);
+  //   });
 
-    // return () => {
-    //   newSocket.close();
-    // };
-  }, []);
+  //   // return () => {
+  //   //   newSocket.close();
+  //   // };
+  // }, []);
 
 
   if (!roomData) return
